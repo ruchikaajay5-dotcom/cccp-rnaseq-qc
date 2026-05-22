283d8a9aeb6b146a83b9afb973ea8de4ec149b3b
Ruchika Ajay <ruchikaajay5@gmail.com.com>
2026-05-22 21:49:30 +0530

    Updated script for QC of 12h timepoint

--- scripts/qc_pipeline.sh
+++ scripts/qc_pipeline.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-fastqc data/6h_cccp/*.fastq.gz -o results/fastqc/6h_CCCP
+mkdir -p results/fastqc/6h_cccp
+mkdir -p results/fastqc/6h_DMSO
+mkdir -p results/fastqc/12h_cccp
+mkdir -p results/fastqc/12h_dmso
+mkdir -p results/multiqc
+
+fastqc data/6h_cccp/*.fastq.gz -o results/fastqc/6h_cccp
 
 fastqc data/6h_DMSO/*.fastq.gz -o results/fastqc/6h_DMSO
 
-multiqc results/fastqc -o results/multiqc
+fastqc data/12h_cccp/*.fastq.gz -o results/fastqc/12h_cccp
 
+fastqc data/12h_DMSO/*.fastq.gz -o results/fastqc/12h_dmso
+
+multiqc results/fastqc -o results/multiqc
